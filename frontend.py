414ddf96213369d3e33c09f12d907adf37fa87ef
Zhu Tao <zhutao.iscas@gmail.com>
2017-04-11 21:32:15 +0800

    added cache to avoid the duplicated prediction

--- frontend.py
+++ frontend.py
@@ -1,11 +1,44 @@
 import os
 import captcha_cnn
-from flask import Flask, render_template, jsonify, request
+import video_classifier
+from flask import Flask, render_template, jsonify, request, flash
+from flask import redirect, url_for
+from flask import send_from_directory
+import uuid
+from werkzeug.utils import secure_filename
+from werkzeug import SharedDataMiddleware
+
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = "upload/"
+app.config['SECRET_KEY'] = "upload/"
+app.wsgi_app = SharedDataMiddleware(app.wsgi_app, {
+        '/upload':  app.config['UPLOAD_FOLDER']
+})
 
 @app.route("/")
 def captcha():
     return render_template("playground.html")
+
+@app.route("/video/", methods=["POST", "GET"])
+def video():
+    if request.method == "POST":
+        if 'video' not in request.files:
+            flash("No file apart")
+            return redirect(request.url)
+        video = request.files["video"]
+        if video and video.filename.endswith("mp4"):
+            filename = secure_filename("{}.mp4".format(uuid.uuid4()))
+            video.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('result', filename=filename))
+    return render_template("video.html")
+
+@app.route("/result/")
+def result():
+    filename = request.args.get("filename")
+    video = os.path.join(app.config["UPLOAD_FOLDER"], filename)
+    stats = video_classifier.analyze_video(str(video))
+    summary = video_classifier.get_summary(stats)
+    return render_template("result.html", summary=summary, video=video)
 
 
 @app.route("/gencaptcha")
